ccfa8238a2ef253d452590e76a42962a0f6181b9
Matyas Selmeci <matyas@cs.wisc.edu>
2017-02-08 18:22:51 -0600

    Add Dockerfile.in and update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+set -eu
+DATE=$(date +%Y%m%S)
+
+sed -e '1i\
+# GENERATED - EDIT Dockerfile.in INSTEAD' \
+    -e "s/@BUILDDATE@/$DATE/" \
+    Dockerfile.in > Dockerfile
+
+git add Dockerfile
+git commit -m "New build at $DATE"
